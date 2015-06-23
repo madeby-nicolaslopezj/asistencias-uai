4ee00303933969dffc1583e2515bfc7789575967
Nicolás López <nicolaslopezj@me.com>
2015-06-23 17:48:26 -0300

    errores

--- routes.js
+++ routes.js
@@ -1,20 +1,8 @@
 Router.map(function() {
   
-  if (Meteor.isCordova) {
-    this.route('courses.index', {
-      path: '/',
-      layoutTemplate: 'layout'
-    });
-
-    this.route('courses.show', {
-      path: '/course/:_id',
-      layoutTemplate: 'layout'
-    });
-  } else {
-    this.route('home', {
-      path: '/'
-    });
-  }
+  this.route('home', {
+    path: '/'
+  });
 
   this.route('collections.students.asistance', {
     path: '/students/:_id/asistance',
