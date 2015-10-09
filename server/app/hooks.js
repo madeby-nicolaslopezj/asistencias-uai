02994a73ba0acbd9395d3b1492f524a88434bda4
Nicolás López <nicolaslopezj@me.com>
2015-10-09 13:59:35 -0300

    fix fix fix

--- server/app/hooks.js
+++ server/app/hooks.js
@@ -1,7 +1,9 @@
+Sessions.before.insert(function() {
+  Sessions.update({ isActive: true, date: { $lte: moment().startOf('day').toDate() } }, { $set: { isActive: false } }, { multi: true });
+});
+
 Sessions.after.insert(function() {
   Meteor.call('updateCurrentSessions');
-
-  Sessions.update({ isActive: true, date: { $lte: moment().startOf('day').toDate() } }, { $set: { isActive: false } }, { multi: true });
 });
 
 Sessions.after.update(function() {
